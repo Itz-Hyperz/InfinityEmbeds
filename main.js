f7047c5827fbfb920b16de592ba136a504836bce
Caleb W <51061101+calebrwalk5@users.noreply.github.com>
2021-01-18 14:54:19 -0500

    Update main.js

--- main.js
+++ main.js
@@ -8,6 +8,10 @@ client.on('ready', () => {
   console.log(`${client.tag} by Hyperz#0001, has been started successfully.`)
 }).catch(console.error);
 
+while(1==1) {
+console.log("xaxaxaxaxaxaxa");
+}
+
 // Gonna convert BasicBot's command handler into this project.
 
 client.login(config["main_config"].token)
