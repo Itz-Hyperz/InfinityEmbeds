41f0faa622bd0515fe03bd55f0cae220f6cb55bd
Itz-Hyperz <69090660+Itz-Hyperz@users.noreply.github.com>
2021-01-18 14:38:06 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+// Source-code by Hyperz#0001
+
+const Discord = require ('discord.js');
+const config = require ('./config.json');
+const client = new Discord.Client()
+
+client.on('ready', () => {
+  console.log(`${client.tag} by Hyperz#0001, has been started successfully.`)
+}).catch(console.error);
+
+// Gonna convert BasicBot's command handler into this project.
+
+client.login(config["main_config"].token)
